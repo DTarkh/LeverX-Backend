71699ddbf3e0aec56ffcba3139dfd452d66501e2
David Tarkhnishvili <datotarkh@gmail.com>
2025-12-17 12:10:55 +0400

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ initDb();
 
 
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`listening to port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
 });
